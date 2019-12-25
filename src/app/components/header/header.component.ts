22294342aff59c3287aac97dff272b22c9dc5c85
subifei123 <17621070228@163.com>
2019-12-25 23:27:38 +0800

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,6 +14,42 @@ export class HeaderComponent implements OnInit {
   }
   msg:any
 
+  public userlist:any[] = [{
+    user:"张三",
+    age: 20
+  },
+{
+  user: "李四",
+  age: 34
+}]
+
+public cars:any[] = [{
+  car: "宝马",
+  list:[
+    {
+      title: "宝马1",
+      price: 30
+    },
+    {
+      title: "宝妈2",
+      price: 40
+    }
+  ]
+},
+{
+  car: "奥迪",
+  list:[{
+    title: "奥迪1",
+    price: 50
+  },{
+    title: "奥迪2",
+    price: 60
+  }]
+}]
+
+
+title = "<h1>插入一个HTML属性</h1>"
+
   constructor() {
     this.msg = "gaibianshuxingdezhi"
    }
