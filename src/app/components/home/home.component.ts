206f18d378128dc9fd913f88d3f1477fb9ae56d8
subifei123 <17621070228@163.com>
2019-12-26 16:04:00 +0800

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public picurl = "http://www.baidu.com/img/bd_logo1.png?where=super";
+
+  public list:any[] = [
+    {
+      title: "news1"
+    },
+    {
+      title: "news2"
+    },{
+      title: "news3"
+    }
+  ];
+
+  public flag: boolean=false;
+  public status: number = 3;
   constructor() { }
 
   ngOnInit() {
